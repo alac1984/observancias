95243642014167c6de64128fa6b8cadc7e781be0
André Carvalho <alac1984@gmail.com>
2021-03-21 13:15:40 -0300

    Heroku settings done

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,13 +10,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gbnraf2!lojd1!6u!jb17orp@#+$u#x90x@3we-2egu1x5jzs!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG'))
+print(DEBUG)
+print(type(DEBUG))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [s.strip() for s in os.environ.get('ALLOWED_HOSTS').split(',')]
 
 # Application definition
 
@@ -127,4 +129,13 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-GOOGLE_RECAPTCHA_SECRET_KEY = '6LcZ33UaAAAAACJEMMcXpwFQmfVsHpsg8PSZYHC9'
+GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_RECAPTCHA_SECRET_KEY')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'alac1984@gmail.com'
+EMAIL_HOST_PASSWORD = 'A1k8u2@ndre'
+EMAIL_PORT = '587'
+
+django_heroku.settings(locals())
